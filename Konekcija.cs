fab55570bfaadb00ff7a9a42b7d3e5a26a015f96
Andrej Dragutinovic <andrej.draa@gmail.com>
2022-05-23 20:50:09 +0200

    Add project files.

---
+++ Konekcija.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace E_Dnevnik
+{
+    class Konekcija
+    {
+        static public SqlConnection Connect()
+        {
+            string CS = "Data Source = DESKTOP-D7QNBFQ\\SQLEXPRESS; Initial Catalog = ednevnik; Integrated Security = True";
+            SqlConnection veza = new SqlConnection(CS);
+            return veza;
+        }
+    }
+}
